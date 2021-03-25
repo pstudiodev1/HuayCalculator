ae176b56a1e10df41e49621f8d0def80a5a86e3a
pstudiodev <pstudiodev@gmail.com>
2021-03-25 16:48:53 +0700

    Done on first commit.

---
+++ CalHuay/MainForm.Designer.cs
@@ -0,0 +1,319 @@
+﻿
+namespace CalHuay
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDigit = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt3DigitUpDaily = new System.Windows.Forms.TextBox();
+            this.txt3DigitTodDaily = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt2DigitUpDaily = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt2DigitDownDaily = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt1DigitUpDaily = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt1DigitDownDaily = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtUpEvenOddDaily = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtDownEvenOddDaily = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txt3DigitUpHighLowDaily = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txt2DigitUpHighLowDaily = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtDownHighLowDaily = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDigit
+            // 
+            this.txtDigit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.txtDigit.Location = new System.Drawing.Point(12, 12);
+            this.txtDigit.Name = "txtDigit";
+            this.txtDigit.Size = new System.Drawing.Size(610, 31);
+            this.txtDigit.TabIndex = 0;
+            this.txtDigit.Text = "63271";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "3-digit-up-daily";
+            // 
+            // txt3DigitUpDaily
+            // 
+            this.txt3DigitUpDaily.Location = new System.Drawing.Point(12, 82);
+            this.txt3DigitUpDaily.Name = "txt3DigitUpDaily";
+            this.txt3DigitUpDaily.Size = new System.Drawing.Size(348, 20);
+            this.txt3DigitUpDaily.TabIndex = 2;
+            // 
+            // txt3DigitTodDaily
+            // 
+            this.txt3DigitTodDaily.Location = new System.Drawing.Point(11, 128);
+            this.txt3DigitTodDaily.Name = "txt3DigitTodDaily";
+            this.txt3DigitTodDaily.Size = new System.Drawing.Size(348, 20);
+            this.txt3DigitTodDaily.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "3-digit-tod-daily";
+            // 
+            // txt2DigitUpDaily
+            // 
+            this.txt2DigitUpDaily.Location = new System.Drawing.Point(11, 179);
+            this.txt2DigitUpDaily.Name = "txt2DigitUpDaily";
+            this.txt2DigitUpDaily.Size = new System.Drawing.Size(348, 20);
+            this.txt2DigitUpDaily.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "2-digit-up-daily";
+            // 
+            // txt2DigitDownDaily
+            // 
+            this.txt2DigitDownDaily.Location = new System.Drawing.Point(11, 232);
+            this.txt2DigitDownDaily.Name = "txt2DigitDownDaily";
+            this.txt2DigitDownDaily.Size = new System.Drawing.Size(348, 20);
+            this.txt2DigitDownDaily.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "2-digit-down-daily";
+            // 
+            // txt1DigitUpDaily
+            // 
+            this.txt1DigitUpDaily.Location = new System.Drawing.Point(11, 288);
+            this.txt1DigitUpDaily.Name = "txt1DigitUpDaily";
+            this.txt1DigitUpDaily.Size = new System.Drawing.Size(348, 20);
+            this.txt1DigitUpDaily.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 272);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "1-digit-up-daily";
+            // 
+            // txt1DigitDownDaily
+            // 
+            this.txt1DigitDownDaily.Location = new System.Drawing.Point(11, 338);
+            this.txt1DigitDownDaily.Name = "txt1DigitDownDaily";
+            this.txt1DigitDownDaily.Size = new System.Drawing.Size(348, 20);
+            this.txt1DigitDownDaily.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 322);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "1-digit-down-daily";
+            // 
+            // txtUpEvenOddDaily
+            // 
+            this.txtUpEvenOddDaily.Location = new System.Drawing.Point(391, 82);
+            this.txtUpEvenOddDaily.Name = "txtUpEvenOddDaily";
+            this.txtUpEvenOddDaily.Size = new System.Drawing.Size(348, 20);
+            this.txtUpEvenOddDaily.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(391, 66);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(91, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "up-even-odd-daily";
+            // 
+            // txtDownEvenOddDaily
+            // 
+            this.txtDownEvenOddDaily.Location = new System.Drawing.Point(391, 128);
+            this.txtDownEvenOddDaily.Name = "txtDownEvenOddDaily";
+            this.txtDownEvenOddDaily.Size = new System.Drawing.Size(348, 20);
+            this.txtDownEvenOddDaily.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(391, 112);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(105, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "down-even-odd-daily";
+            // 
+            // txt3DigitUpHighLowDaily
+            // 
+            this.txt3DigitUpHighLowDaily.Location = new System.Drawing.Point(391, 179);
+            this.txt3DigitUpHighLowDaily.Name = "txt3DigitUpHighLowDaily";
+            this.txt3DigitUpHighLowDaily.Size = new System.Drawing.Size(348, 20);
+            this.txt3DigitUpHighLowDaily.TabIndex = 18;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(391, 163);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(116, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "3-digit-up-high-low-daily";
+            // 
+            // txt2DigitUpHighLowDaily
+            // 
+            this.txt2DigitUpHighLowDaily.Location = new System.Drawing.Point(391, 232);
+            this.txt2DigitUpHighLowDaily.Name = "txt2DigitUpHighLowDaily";
+            this.txt2DigitUpHighLowDaily.Size = new System.Drawing.Size(348, 20);
+            this.txt2DigitUpHighLowDaily.TabIndex = 20;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(391, 216);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(116, 13);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "2-digit-up-high-low-daily";
+            // 
+            // txtDownHighLowDaily
+            // 
+            this.txtDownHighLowDaily.Location = new System.Drawing.Point(391, 288);
+            this.txtDownHighLowDaily.Name = "txtDownHighLowDaily";
+            this.txtDownHighLowDaily.Size = new System.Drawing.Size(348, 20);
+            this.txtDownHighLowDaily.TabIndex = 22;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(391, 272);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(99, 13);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "down-high-low-daily";
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btnProcess.Location = new System.Drawing.Point(628, 12);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(112, 31);
+            this.btnProcess.TabIndex = 23;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 387);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.txtDownHighLowDaily);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txt2DigitUpHighLowDaily);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txt3DigitUpHighLowDaily);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtDownEvenOddDaily);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtUpEvenOddDaily);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txt1DigitDownDaily);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt1DigitUpDaily);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt2DigitDownDaily);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt2DigitUpDaily);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt3DigitTodDaily);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt3DigitUpDaily);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDigit);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Huay";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDigit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt3DigitUpDaily;
+        private System.Windows.Forms.TextBox txt3DigitTodDaily;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt2DigitUpDaily;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt2DigitDownDaily;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt1DigitUpDaily;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt1DigitDownDaily;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtUpEvenOddDaily;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtDownEvenOddDaily;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txt3DigitUpHighLowDaily;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txt2DigitUpHighLowDaily;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtDownHighLowDaily;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btnProcess;
+    }
+}
+
---
+++ CalHuay/MainForm.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalHuay
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string twoDigit = txtDigit.Text.Substring(0, 2);
+                string threeDigit = txtDigit.Text.Substring(2, 3);
+                //
+                txt3DigitUpDaily.Text = threeDigit;
+                txt2DigitDownDaily.Text = twoDigit;
+                //
+                txt3DigitTodDaily.Text = calculate3DigitTod(threeDigit);
+                //
+                txt2DigitUpDaily.Text = threeDigit[1].ToString() + threeDigit[2].ToString();
+                //
+                txt1DigitUpDaily.Text = calculate1DigitUpDown(threeDigit);
+                txt1DigitDownDaily.Text = calculate1DigitUpDown(twoDigit);
+                //
+                txtUpEvenOddDaily.Text = ((Convert.ToInt32(threeDigit) % 2) == 0) ? "0" : "1";
+                txtDownEvenOddDaily.Text = ((Convert.ToInt32(twoDigit) % 2) == 0) ? "0" : "1";
+                //
+                txt3DigitUpHighLowDaily.Text = (Convert.ToInt32(threeDigit) < 500) ? "0" : "1";
+                txt2DigitUpHighLowDaily.Text = (Convert.ToInt32(threeDigit[1].ToString() + threeDigit[2].ToString()) < 50) ? "0" : "1";
+                txtDownHighLowDaily.Text = (Convert.ToInt32(twoDigit) < 50) ? "0" : "1";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private string calculate1DigitUpDown(string number)
+        {
+            List<string> digits = new List<string>();
+
+            for(int i=0; i<number.Length; i++)
+            {
+                string n = number[i].ToString();
+                if(digits.Contains(n) == false)
+                {
+                    digits.Add(n);
+                }
+            }
+
+            return string.Join(", ", digits);
+        }
+
+        private string calculate3DigitTod(string threeDigit)
+        {
+            List<string> tod = new List<string>();
+
+            {
+                var item = threeDigit[0].ToString() + threeDigit[1].ToString() + threeDigit[2].ToString();
+                if (tod.Contains(item) == false)
+                {
+                    tod.Add(item);
+                }
+            }
+            {
+                var item = threeDigit[0].ToString() + threeDigit[2].ToString() + threeDigit[1].ToString();
+                if (tod.Contains(item) == false)
+                {
+                    tod.Add(item);
+                }
+            }
+
+            //
+
+            {
+                var item = threeDigit[1].ToString() + threeDigit[0].ToString() + threeDigit[2].ToString();
+                if (tod.Contains(item) == false)
+                {
+                    tod.Add(item);
+                }
+            }
+            {
+                var item = threeDigit[1].ToString() + threeDigit[2].ToString() + threeDigit[0].ToString();
+                if (tod.Contains(item) == false)
+                {
+                    tod.Add(item);
+                }
+            }
+
+            //
+
+            {
+                var item = threeDigit[2].ToString() + threeDigit[1].ToString() + threeDigit[0].ToString();
+                if (tod.Contains(item) == false)
+                {
+                    tod.Add(item);
+                }
+            }
+            {
+                var item = threeDigit[2].ToString() + threeDigit[0].ToString() + threeDigit[1].ToString();
+                if (tod.Contains(item) == false)
+                {
+                    tod.Add(item);
+                }
+            }
+
+            //
+
+            return string.Join(", ", tod);
+        }
+    }
+}
